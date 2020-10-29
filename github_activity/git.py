4a155e41dbebf5d5bc839fe948c9313ff927bd4c
Erik Sundell <erik@sundellopensource.se>
2020-10-29 14:16:34 +0100

    Remove no longer used code

--- github_activity/git.py
+++ github_activity/git.py
@@ -7,12 +7,3 @@ def _git_installed_check():
         return True
     except subprocess.CalledProcessError:
         return False
-
-
-def _valid_git_reference_check(git_ref):
-    cmd = ["git", "rev-parse", "--verify", git_ref]
-    try:
-        subprocess.check_call(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-        return True
-    except subprocess.CalledProcessError:
-        return False
